99d5e52d02926502f0a8353802b9b0ecdc2d17e1
Vladislav Kvasilchuk <vladkvi88@mail.ru>
2026-02-15 13:54:55 +0300

    feat: fifteen puzzle mini app

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true, // чтобы принимать внешний трафик
-    allowedHosts: [
-      "398fhy-95-24-95-140.ru.tuna.am", // твой хост от Tuna
-    ],
-  },
+  base: "/telegram-fifteen/", // имя репозитория
+  // остальной конфиг
 });
